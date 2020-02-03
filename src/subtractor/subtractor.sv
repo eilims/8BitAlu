ed8e3bab78397f55b9ae8c1460f3d942edc2de37
eilims <flyingmonkeys1996@gmail.com>
2020-02-02 19:51:36 -0800

    Add subtractor circuit

---
+++ src/subtractor/subtractor.sv
@@ -0,0 +1,67 @@
+`include "adder.sv"
+`include "twos_comp.sv"
+`include "comparator.sv"
+
+module subtractor
+#(
+	parameter integer SIZE = 1
+)
+(
+	input wire[SIZE-1:0] a,
+	input wire[SIZE-1:0] b,
+	output wire overflow,
+	output wire[SIZE:0] result
+);
+
+wire sign_result_eq;
+wire sign_result_a_g;
+
+wire[SIZE-1:0] b_twos_comp;
+
+wire adder_overflow;
+wire dummy_val;
+
+wire internal_overflow;
+
+comparator
+#(
+	1'b1
+)
+sign_results
+(
+	a[SIZE-1],
+	b[SIZE-1],
+	sign_result_a_g,
+	sign_result_eq
+);
+
+twos_complementor
+#(
+	SIZE
+)
+b_inversion
+(
+	b,
+	b_twos_comp
+);
+
+adder
+#(
+	SIZE+1
+)
+adder_stage
+(
+	{a[SIZE-1], a},
+	{b_twos_comp[SIZE-1], b_twos_comp},
+	adder_overflow,
+	{dummy_val, result}
+);
+
+// If the minuend (a) is positive, we overflow if the result is negative
+// If the minuend (a) is negative, we overflow if the the adder overflows
+assign internal_overflow = (a[SIZE-1]) ? adder_overflow : result[SIZE-1];
+// If the minuend (a) and the subtrahend (b) are opposite signs the result
+// cannot overflow
+assign overflow = (sign_result_eq) ? 1'b0 : internal_overflow;
+
+endmodule
---
+++ tst/subtractor/subtractor_tb.sv
@@ -0,0 +1,134 @@
+`include "subtractor.sv"
+`include "test_functions.sv"
+
+module subtractor_tb();
+
+localparam integer SIZE = 4;
+reg[SIZE-1:0] a;
+reg[SIZE-1:0] b;
+wire overflow;
+wire[SIZE:0] result;
+
+initial
+begin
+	$dumpfile("subtractor_4.vvp");
+        $dumpvars(0, subtractor_tb);
+        $display("Time\t a\t b\t over\t result");
+        $monitor("%g\t %b\t %b\t %b\t %b",$time,a,b,overflow,result);
+
+	//Positive number tests
+	a = 4'h0;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h00);
+
+	#1
+	a = 4'h1;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h01);
+
+	#1
+	a = 4'h1;
+	b = 4'h1;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h00);
+
+	#1
+	a = 4'h5;
+	b = 4'h3;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h02);
+
+	#1
+	a = 4'h7;
+	b = 4'h2;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h05);
+
+	#1
+	a = 4'h5;
+	b = 4'h6;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h1F);
+	
+	// Negative number tests
+	#1
+	a = 4'h9; // -7
+	b = 4'hE; // -2
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h1B); // -5
+
+	#1
+	a = 4'hF; // -1
+	b = 4'hF; // -1
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h00);
+
+	#1
+	a = 4'hE; // -2
+	b = 4'hB; // -5
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 5'h03);
+
+	// Overflow testing
+	#1
+	a = 4'hB; // -5
+	b = 4'h4;
+	#1
+	`ASSERT(overflow, 1'b1);
+	`ASSERT(result, 5'h17); // "-9" but invalid
+
+	#1
+	a = 4'hC; // -4
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b1);
+	`ASSERT(result, 5'h17); // "-9" but invalid
+
+	#1
+	a = 4'h4;
+	b = 4'hB; // -5
+	#1
+	`ASSERT(overflow, 1'b1);
+	`ASSERT(result, 5'h09); // "9" but invalid
+
+	#1
+	a = 4'h1;
+	b = 4'h8; // -8
+	#1
+	`ASSERT(overflow, 1'b1);
+	`ASSERT(result, 5'h19); // "9" but invalid
+
+	#1
+	a = 4'h1;
+	b = 4'h9; // -7
+	#1
+	`ASSERT(overflow, 1'b1);
+	`ASSERT(result, 5'h08); // "8" but invalid
+
+	#1 $finish;
+end
+
+subtractor
+#(
+	SIZE
+)
+uut
+(
+	a,
+	b,
+	overflow,
+	result
+);
+
+endmodule
