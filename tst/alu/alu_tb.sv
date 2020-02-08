9738a38933da69ee8bf30ec23d9abc7f7dfbf277
eilims <flyingmonkeys1996@gmail.com>
2020-02-08 10:31:30 -0800

    Add tests for basic functions: AND, OR, XOR, NOT

--- tst/alu/alu_tb.sv
+++ tst/alu/alu_tb.sv
@@ -18,6 +18,174 @@ begin
 	$dumpvars(0, alu_tb);
 	$display("Time\t en\t com\t a\t b\t over\t r\t");
 	$monitor("%g\t %b\t %b\t %b\t %b\t %b\t %b\t",$time,enable,command,a,b,overflow,result);
+
+	// AND Test
+	$display("AND Test");
+	enable = 1'b1;
+	command = 4'b0;
+	a = 4'h1;
+	b = 4'h1;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h1);
+
+	enable = 1'b1;
+	command = 4'b0;
+	a = 4'h5;
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h5);
+
+	enable = 1'b1;
+	command = 4'b0;
+	a = 4'hF;
+	b = 4'hF;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'hF);
+
+	enable = 1'b1;
+	command = 4'b0;
+	a = 4'hA;
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h0);
+
+	enable = 1'b1;
+	command = 4'b0;
+	a = 4'h7;
+	b = 4'h3;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h3);
+
+	// OR Test
+	$display("OR Test");
+	enable = 1'b1;
+	command = 4'h1;
+	a = 4'h1;
+	b = 4'h1;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h1);
+
+	enable = 1'b1;
+	command = 4'h1;
+	a = 4'h5;
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h5);
+
+	enable = 1'b1;
+	command = 4'h1;
+	a = 4'hF;
+	b = 4'hF;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'hF);
+
+	enable = 1'b1;
+	command = 4'h1;
+	a = 4'hA;
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'hF);
+
+	enable = 1'b1;
+	command = 4'h1;
+	a = 4'h7;
+	b = 4'h3;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h7);
+
+	// XOR Test
+	$display("XOR Test");
+	enable = 1'b1;
+	command = 4'h2;
+	a = 4'h1;
+	b = 4'h1;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h0);
+
+	enable = 1'b1;
+	command = 4'h2;
+	a = 4'h5;
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h0);
+
+	enable = 1'b1;
+	command = 4'h2;
+	a = 4'hF;
+	b = 4'hF;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h0);
+
+	enable = 1'b1;
+	command = 4'h2;
+	a = 4'hA;
+	b = 4'h5;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'hF);
+
+	enable = 1'b1;
+	command = 4'h2;
+	a = 4'h7;
+	b = 4'h3;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h4);
+
+	// NOT Test
+	$display("NOT Test");
+	enable = 1'b1;
+	command = 4'h3;
+	a = 4'h1;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'hE);
+
+	enable = 1'b1;
+	command = 4'h3;
+	a = 4'h5;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'hA);
+
+	enable = 1'b1;
+	command = 4'h3;
+	a = 4'hF;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h0);
+
+	enable = 1'b1;
+	command = 4'h3;
+	a = 4'hA;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h5);
+
+	enable = 1'b1;
+	command = 4'h3;
+	a = 4'h7;
+	b = 4'h0;
+	#1
+	`ASSERT(overflow, 1'b0);
+	`ASSERT(result, 4'h8);
 end
 
 alu
